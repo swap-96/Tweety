43cbf8536fad9ec268de71c00201857ed1f7bff0
Swapnil Ghosh <gswapnil100@gmail.com>
2020-06-18 19:36:42 +0530

    Can Tweet now

--- reply.py
+++ reply.py
@@ -1,11 +1,23 @@
+import random
 import tweepy
+import os
+
+# DO NOT COMMIT KEYS!!!!
+API_KEY = os.environ.get('API_KEY')
+API_SECRET = os.environ.get('API_SECRET')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Create API object
 api = tweepy.API(auth)
-
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
 # Create a tweet
 api.update_status("Hello Tweepy")
